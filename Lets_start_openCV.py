045f11868b42d409bb154b5da037c70d07c9cdf5
Shahriar <shahriarhajiabbasi@gmail.com>
2021-02-11 23:17:58 +0330

    end of part 2 of tutorial and learning how to manipulate image

--- Lets_start_openCV.py
+++ Lets_start_openCV.py
@@ -1,4 +1,5 @@
 import cv2
+import random
 
 img = cv2.imread('assets/logo.png', -1)
 # resize image in specific resize
@@ -14,5 +15,28 @@ cv2.imwrite('new_rotate.png', img_half)
 cv2.imshow('Coffee Logo', img)
 cv2.imshow('Coffee Logo in half', img_half)
 cv2.imshow("our logo rotated", im_rotate)
+cv2.waitKey(1000)
+cv2.destroyAllWindows()
+
+# Image representation in BGR format not RGB :)
+print(img)
+print(type(img))
+# number of height and width
+print(img.shape)
+# print  row of Image
+print(img[150][15:128])
+
+# manipulate image WITH RANDOM COLOR
+for i in range(300):
+    for j in range(img.shape[1]):
+        img[i][j] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+cv2.imshow(' Im show ', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Copy and paste part of image
+tag = img[300:400, 100:200]
+img[100:200, 300:400] = tag
+cv2.imshow(' replace Im show ', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
