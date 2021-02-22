f1919235e5c208191d9fe6893d4cea6bd1fddb88
Shahriar <shahriarhajiabbasi@gmail.com>
2021-02-22 16:17:34 +0330

    show the mask and result together

--- 5_color_detection.py
+++ 5_color_detection.py
@@ -18,7 +18,9 @@ while True:
 
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
-    cv2.imshow('frame', result)
+    cv2.imshow('Result', result)
+    cv2.imshow('mask', mask)
+
 
     if cv2.waitKey(1) == ord('q'):
         break
